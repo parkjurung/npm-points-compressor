a3a85afecab082950e86c839e06a5334107e50b1
Jurung Park <jurung@pplink.net>
2018-06-15 19:19:51 +0900

    clean up index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,1 @@
-import {numToString, stringToNum} from './src';
-
-const testNumbers = [123.0011, 4141.1044, 1923.3, 13444.0003];
-
-testNumbers.forEach(num => {
-  console.log('original', num);
-  const numToStringResult = numToString(num);
-  console.log('numToStringResult', numToStringResult);
-  const stringToNumResult = stringToNum(numToStringResult);
-  console.log('stringToNumResult', stringToNumResult);
-});
-
 export * from './src';
